09453e30fa0e2c6123a7136305dbaaa020dcc7de
chacoff <jaime.arctico@gmail.com>
2024-11-15 18:43:09 +0100

    getting project ID based on the task_id

--- ScrapCharts/plugin.py
+++ ScrapCharts/plugin.py
@@ -30,6 +30,7 @@ class Plugin(PluginBase):
             x_values, y_values, updated_at_values, flights, task_id = get_data_from_db()
             label = "Volume [m³]"
             projects_tasks = get_projects_with_tasks()
+            project_id = get_project_id_from_task_id(projects_tasks, task_id)
 
             template_args = {
                 'x_values': x_values,
@@ -40,6 +41,7 @@ class Plugin(PluginBase):
                 'n_piles': len(x_values),
                 'flights': flights,
                 'task_id': task_id,
+                'task_project_id': project_id,
                 'projects_with_tasks': projects_tasks
             }
 
@@ -49,10 +51,13 @@ class Plugin(PluginBase):
         def get_flight_data(request):
             flight_day = request.GET.get("flightDay", "")
             x_values, y_values, updated_at_values, _, task_id = get_data_from_db(flight_day)
+            projects_tasks = get_projects_with_tasks()
+            project_id = get_project_id_from_task_id(projects_tasks, task_id)
             return JsonResponse({"x_values": list(x_values),
                                  "y_values": list(y_values),
                                  "updated_at_values": list(updated_at_values),
-                                 "task_id": task_id})
+                                 "task_id": task_id,
+                                 "task_project_id": project_id})
 
         return [
             MountPoint('$', volume_graphs), 
@@ -109,3 +114,20 @@ def get_projects_with_tasks():
         })
 
     return projects_with_tasks
+
+
+def get_project_id_from_task_id(_projects_tasks: list, _task_id: str) -> int:
+
+    for project_data in _projects_tasks:
+        project = project_data['project']
+        tasks = project_data['tasks']
+
+        for task in tasks:
+            task_id: str = task.id  # it is a class uuid.UUID
+            project_id: int = task.project_id
+            if str(task_id) == _task_id:
+                print(f'Task ID: {task_id}, Project ID: {project_id}')
+                return project_id
+
+    return 0
+
