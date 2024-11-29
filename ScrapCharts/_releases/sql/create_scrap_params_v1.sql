ba0096769d9921ddaec73ea730c7bab4d7de9238
chacoff <jaime.arctico@gmail.com>
2024-11-29 15:29:09 +0100

    added sql example for scrap_params table

---
+++ ScrapCharts/_releases/sql/create_scrap_params_v1.sql
@@ -0,0 +1,13 @@
+CREATE TABLE public.SCRAP_PARAMS (
+    counter SERIAL PRIMARY KEY,
+    sector VARCHAR(20) NOT NULL,
+    angle INTEGER,
+    crop_left INTEGER,
+    crop_top INTEGER,
+    crop_right INTEGER,
+    crop_bottom INTEGER,
+    scale FLOAT,
+    quality INTEGER,
+    reviewer VARCHAR(40),
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
