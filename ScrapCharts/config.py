9c13889b1daffe4c965d3a91698f5edc653f37c1
chacoff <jaime.arctico@gmail.com>
2025-02-08 22:15:25 +0100

    removed .env

--- ScrapCharts/config.py
+++ ScrapCharts/config.py
@@ -1,19 +1,15 @@
-import os
 from dataclasses import dataclass
-from dotenv import load_dotenv
 
 
 @dataclass()
 class Config:
     _dev: bool = True
-    DB_DEV: str = 'DB_DEV'
-    DB_PROD: str = 'DB_PROD'
+    DB_DEV: str = 'postgresql+psycopg2://postgres:API@host.docker.internal:5432/scrap'
+    DB_PROD: str = 'postgresql+psycopg2://postgres:ArcelorT3ch*2024!?@host.docker.internal:5432/Volumes_DB'
 
-    def db_url(self, ):
-        load_dotenv('.env')
+    def db_url(self, ) -> str:
         _db: str = self.DB_DEV if self._dev else self.DB_PROD
-
-        return os.getenv(_db)
+        return _db
 
 # Reminder:
 # localhost // docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' db
